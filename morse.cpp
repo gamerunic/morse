708c8d302a7f372f6aaa52cddf034e572a8bbd97
gamerunic <135882520+gamerunic@users.noreply.github.com>
2024-02-17 00:25:30 +0100

    Add files via upload

--- morse.cpp
+++ morse.cpp
@@ -1,30 +1,36 @@
 #include <iostream>
+#include <string>
 #include <fstream>
 #include <algorithm>
 using namespace std;
 
 int main()
 {
-    string alfabeto, frase_iniziale, frase_finale = " ",parte_morse = " ";
-    size_t pos;
-    int indice = 0;
-    char lettera_salvata;
+    string alfabeto, frase_iniziale, frase_finale, parte_morse;
+    int pos = 0;
+    int indice = 1;
     ifstream frase_input("testo.txt");
     ifstream input_file("alfabeto.txt");
     getline(frase_input, frase_iniziale);
-    while (getline(input_file, alfabeto, ' '))
+    getline(input_file, alfabeto);
+    for (char carattere : frase_iniziale)
     {
-        for (char carattere : frase_iniziale)
+        if (carattere != ' ')
         {
+            pos = 0;
+            indice = 1;
             pos = alfabeto.find(carattere);
-            while (alfabeto[pos-indice] != ' ')
+            while (alfabeto[pos+indice] != carattere)
             {
-                parte_morse += alfabeto[pos-indice];
+                parte_morse += alfabeto[pos+indice];
                 indice += 1;
             }
-            reverse(parte_morse.begin(), parte_morse.end());
-            frase_finale += parte_morse;
+        }
+        else
+        {
+            parte_morse += '|';
         }
     }
+    frase_finale += parte_morse;
     cout<<frase_finale;
 }
